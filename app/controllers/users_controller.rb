b42a1cf498bd1e25b39e968b27dc071c11794ae6
Tatjana Tanaskovic <tatjanatanaskovich@gmail.com>
2017-12-12 18:45:49 +0000

    Add sign in upon sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:edit, :update, :show]
   before_action :require_same_user, only: [:edit, :update]
+  
   def index
     @users = User.paginate(page: params[:page], per_page: 3)
   end
@@ -12,8 +13,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "Welcome to the alpha blog #{@user.username}"
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else
       render 'new'
     end
