e81c18f7a6c510064374d7ffa7c392c0f5d1ec95
Tatjana Tanaskovic <tatjanatanaskovich@gmail.com>
2017-12-13 18:23:08 +0000

    Add admin requirement to create categories

--- test/integration/list_categories_test.rb
+++ test/integration/list_categories_test.rb
@@ -13,5 +13,4 @@ class ListCategoriesTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", category_path(@category), text: @category.name
     assert_select "a[href=?]", category_path(@category2), text: @category2.name
   end
-  
 end
